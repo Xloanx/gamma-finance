ed95dc59e8fb8276179d4048bd87ebd15d946acf
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-03-08 15:32:28 +0100

    increase typing speed

--- components/typingIndicator.jsx
+++ components/typingIndicator.jsx
@@ -8,7 +8,7 @@ const TypingIndicator = () => {
           key={i}
           className="w-2 h-2 bg-gray-400 rounded-full"
           animate={{ opacity: [0.3, 1, 0.3] }}
-          transition={{ duration: 1, repeat: Infinity, delay: i * 0.2 }}
+          transition={{ duration: 1, repeat: Infinity, delay: i * 0.1 }}
         ></motion.span>
       ))}
     </div>
